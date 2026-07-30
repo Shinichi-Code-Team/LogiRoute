90d76216078622781212921c2bfd2b620094c616
wlaa <wlaamasri95@gmail.com>
2026-07-30 16:59:37 +0300

    feat: create Warehouse domain class with core fields

--- src/main/kotlin/com/example/logiroute/domain/model/Route.kt
+++
@@ -1,4 +0,0 @@
-package com.example.logiroute.domain.model
-
-class Route {
-}
--- src/main/kotlin/com/example/logiroute/domain/model/Warehouse.kt
+++ src/main/kotlin/com/example/logiroute/domain/model/Warehouse.kt
@@ -1,20 +1,9 @@
 package com.example.logiroute.domain.model
 
-class Warehouse (
+data git addclass Warehouse (
     val id: String,
     val name: String,
-    val regionalZone: String
-){
-  private val _cargoQueue : MutableList<Package> = mutableListOf()
-    val cargoQueue : List<Package> get() = _cargoQueue
-
-    private val _outgoingRoutes : MutableList<Route> = mutableListOf()
-  val outgoingRoutes get() = _outgoingRoutes
-
-    internal fun addPackage(pkg: Package){
-        _cargoQueue.add(pkg)
-    }
-  internal fun addRoute(route: Route) {
-      _outgoingRoutes.add(route)
-  }
-}
+    val regionalZone: String,
+    val latitude: Double,
+    val longitude: Double
+)
