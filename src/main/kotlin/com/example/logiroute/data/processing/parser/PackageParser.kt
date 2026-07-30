c54db1e6a418a3d61e3f12e34eb993c41189bcce
Ghada Alsaqqa <ghadaosama835@gmail.com>
2026-07-30 21:51:33 +0300

    feat: add domain models, warehouse encapsulation, and data loaderss

--- src/main/kotlin/com/example/logiroute/data/processing/parser/PackageParser.kt
+++ src/main/kotlin/com/example/logiroute/data/processing/parser/PackageParser.kt
@@ -1,14 +1,13 @@
 package com.example.logiroute.data.processing.parser
 
 import com.example.logiroute.data.dataholder.PackageRaw
-import com.example.logiroute.data.processing.validation.INVALID_DOUBLE_VALUE
-import com.example.logiroute.data.processing.validation.hasExpectedColumnCount
-import com.example.logiroute.data.processing.validation.parsePositiveDoubleOrInvalid
+import com.example.logiroute.data.processing.validation.*
 
 private const val PACKAGE_ID_INDEX = 0
 private const val PACKAGE_WEIGHT_INDEX = 1
-private const val DESTINATION_HUB_ID_INDEX = 2
-private const val PACKAGE_PRIORITY_INDEX = 3
+private const val ORIGIN_HUB_ID_INDEX = 2
+private const val DESTINATION_HUB_ID_INDEX = 3
+private const val PACKAGE_PRIORITY_INDEX = 4
 
 fun parsePackages(lines: List<String>): List<PackageRaw> {
     if (lines.isEmpty()) {
@@ -36,7 +35,15 @@ fun parsePackages(lines: List<String>): List<PackageRaw> {
             continue
         }
 
-        packages.add(buildPackageRaw(columns))
+        packages.add(
+            PackageRaw(
+                id = columns[PACKAGE_ID_INDEX],
+                weight = parsePositiveDoubleOrInvalid(columns[PACKAGE_WEIGHT_INDEX]),
+                originHubId = columns[ORIGIN_HUB_ID_INDEX],
+                destinationHubId = columns[DESTINATION_HUB_ID_INDEX],
+                priority = parsePriority(columns[PACKAGE_PRIORITY_INDEX])
+            )
+        )
     }
 
     return packages
@@ -81,13 +88,14 @@ private fun isValidPackageWeight(weight: String): Boolean {
     return parsePositiveDoubleOrInvalid(weight) != INVALID_DOUBLE_VALUE
 }
 
-private fun buildPackageRaw(
-    columns: List<String>
-): PackageRaw {
-    return PackageRaw(
-        id = columns[PACKAGE_ID_INDEX],
-        weight = parsePositiveDoubleOrInvalid(columns[PACKAGE_WEIGHT_INDEX]),
-        destinationHubId = columns[DESTINATION_HUB_ID_INDEX],
-        priority = parsePriority(columns[PACKAGE_PRIORITY_INDEX])
-    )
-}
+//private fun buildPackageRaw(
+//    columns: List<String>
+//): PackageRaw {
+//    return PackageRaw(
+//        id = columns[PACKAGE_ID_INDEX],
+//        weight = parsePositiveDoubleOrInvalid(columns[PACKAGE_WEIGHT_INDEX]),
+//        originHubId = columns[ORIGIN_HUB_ID_INDEX],
+//        destinationHubId = columns[DESTINATION_HUB_ID_INDEX],
+//        priority = parsePriority(columns[PACKAGE_PRIORITY_INDEX])
+//    )
+//}
