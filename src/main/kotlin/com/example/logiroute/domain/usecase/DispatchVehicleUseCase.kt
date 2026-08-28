d75ab8ed9c7640a67f94c329ad224511281d51dd
Fathi F.F Alzard <fathifuad115@gmial.com>
2026-08-28 19:15:41 +0300

    create DispatchVehicleUseCase class

---
+++ src/main/kotlin/com/example/logiroute/domain/usecase/DispatchVehicleUseCase.kt
@@ -0,0 +1,24 @@
+package com.example.logiroute.domain.usecase
+
+import com.example.logiroute.domain.model.Package
+import com.example.logiroute.domain.model.Vehicle
+import com.example.logiroute.domain.model.Warehouse
+
+class DispatchVehicleUseCase {
+
+    operator fun invoke(warehouse: Warehouse, vehicle: Vehicle): List<Package> {
+        val availablePackages = warehouse.cargoQueue
+        val (loadedPackages, _) = availablePackages.fold(
+            initial = emptyList<Package>() to 0.0
+        ) { (accPackages, currentWeight), pkg ->
+            val nextWeight = currentWeight + pkg.weight
+            if (nextWeight <= vehicle.maxCapacityKg) {
+                (accPackages + pkg) to nextWeight
+            } else {
+                accPackages to currentWeight
+            }
+        }
+
+        return loadedPackages
+    }
+}
