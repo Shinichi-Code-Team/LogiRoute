87bca3be77dd0b2675abd86b64fc1e4295689d8f
Deema-AL-Maqadma <dema.maqadma@gmail.com>
2026-08-30 16:14:17 +0300

    feat(request): rename data classes request

--- src/main/kotlin/com/example/logiroute/domain/model/request/ConsolidationPlanRequest.kt
+++ src/main/kotlin/com/example/logiroute/domain/model/request/ConsolidationPlanRequest.kt
@@ -1,9 +1,9 @@
-package com.example.logiroute.com.example.logiroute.domain.model
+package com.example.logiroute.com.example.logiroute.domain.model.request
 
 import com.example.logiroute.domain.model.Package
 import com.example.logiroute.domain.model.Vehicle
 
-data class ConsolidationPlan(
+data class ConsolidationPlanRequest(
     val vehicle: Vehicle,
     val selectedPackages: List<Package>,
     val totalWeight: Double,
