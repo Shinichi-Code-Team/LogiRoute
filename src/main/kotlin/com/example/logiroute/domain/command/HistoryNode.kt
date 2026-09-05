a6d2ef56b95f438e820fa8437a85e6203a9afbc1
Ghada Alsaqqa <ghadaosama835@gmail.com>
2026-09-05 06:01:54 +0300

    Add branching tree command history

--- src/main/kotlin/com/example/logiroute/domain/command/HistoryNode.kt
+++ src/main/kotlin/com/example/logiroute/domain/command/HistoryNode.kt
@@ -1,3 +1,7 @@
 package com.example.logiroute.domain.command
 
-data class HistoryNode()
+data class HistoryNode(
+    val command: Command?,
+    val parent: HistoryNode?,
+    val children: MutableList<HistoryNode> = mutableListOf()
+)
