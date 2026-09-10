7e2c4044302dc1167905c4663df5814d04b3f012
wlaa <wlaamasri95@gmail.com>
2026-09-10 16:28:22 +0300

    refactor: replace magic numbers with named constants

--- src/main/kotlin/com/example/logiroute/domain/usecase/CalculateVehicleUtilizationUseCase.kt
+++ src/main/kotlin/com/example/logiroute/domain/usecase/CalculateVehicleUtilizationUseCase.kt
@@ -6,19 +6,26 @@ import com.example.logiroute.domain.usecase.model.exceptions.LogisticsException
 
 class CalculateVehicleUtilizationUseCase {
 
+    private companion object {
+        const val MIN_VALID_CAPACITY_KG = 0.0
+        const val PERCENTAGE_MULTIPLIER = 100.0
+    }
+
     operator fun invoke(
         vehicle: Vehicle
     ): VehicleUtilization {
-        if (vehicle.maxCapacityKg <= 0.0) {
+        if (vehicle.maxCapacityKg <= MIN_VALID_CAPACITY_KG) {
             throw LogisticsException.InvalidCapacityException(vehicle.maxCapacityKg)
         }
 
         val currentLoad = vehicle.loadedPackages
-                .sumOf { it.weight }
+            .sumOf { it.weight }
 
-        val remainingCapacity = vehicle.maxCapacityKg - currentLoad
+        val remainingCapacity =
+            vehicle.maxCapacityKg - currentLoad
 
-        val utilizationPercentage = (currentLoad / vehicle.maxCapacityKg) * 100
+        val utilizationPercentage =
+            (currentLoad / vehicle.maxCapacityKg) * PERCENTAGE_MULTIPLIER
 
         return VehicleUtilization(
             currentLoadKg = currentLoad,
