a51748390e35a40f4d8819699e5c1545e47b8cb1
Mohammed-Alhams <mohammed1212alhams@gmail.com>
2026-09-10 13:59:50 +0300

    happy intro to KMP ya Deema "D

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,36 @@
+rootProject.name = "LogiRoute"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google {
+            mavenContent {
+                includeGroupAndSubgroups("androidx")
+                includeGroupAndSubgroups("com.android")
+                includeGroupAndSubgroups("com.google")
+            }
+        }
+        mavenCentral()
+    }
+}
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "LogiRoute"
+
+include(":frontend:logiroute")
+
+project(":frontend:logiroute").projectDir = file("frontend/logiroute")
