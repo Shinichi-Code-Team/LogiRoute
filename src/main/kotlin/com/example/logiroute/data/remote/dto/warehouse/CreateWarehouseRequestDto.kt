5b16fea012b336b73d65438cb36d7b89acb9d151
Musaab-Hisham <mosaabhisham2006@gmail.com>
2026-09-16 15:23:41 +0300

    feat: add warehouse crud request dtos

--- src/main/kotlin/com/example/logiroute/data/remote/dto/warehouse/CreateWarehouseRequestDto.kt
+++ src/main/kotlin/com/example/logiroute/data/remote/dto/warehouse/CreateWarehouseRequestDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class WarehouseResponseDto(
+data class CreateWarehouseRequestDto(
     val id: String,
     val name: String,
     @SerialName("regional_zone")
