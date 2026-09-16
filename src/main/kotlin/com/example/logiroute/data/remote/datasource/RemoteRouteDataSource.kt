0a1804eea51e5f408687841936b6ab9a0add3549
Musaab-Hisham <mosaabhisham2006@gmail.com>
2026-09-16 06:00:16 +0300

    feat: add route crud remote interface

--- src/main/kotlin/com/example/logiroute/data/remote/datasource/RemoteRouteDataSource.kt
+++ src/main/kotlin/com/example/logiroute/data/remote/datasource/RemoteRouteDataSource.kt
@@ -1,7 +1,21 @@
 package com.example.logiroute.data.remote.datasource
 
+import com.example.logiroute.data.remote.dto.route.CreateRouteRequestDto
 import com.example.logiroute.data.remote.dto.route.RouteResponseDto
+import com.example.logiroute.data.remote.dto.route.UpdateRouteRequestDto
 
 interface RemoteRouteDataSource {
+
     fun getRoutes(): List<RouteResponseDto>
+
+    fun getRouteById(id: String): RouteResponseDto?
+
+    fun createRoute(request: CreateRouteRequestDto): RouteResponseDto
+
+    fun updateRoute(
+        id: String,
+        request: UpdateRouteRequestDto
+    ): RouteResponseDto
+
+    fun deleteRoute(id: String)
 }
