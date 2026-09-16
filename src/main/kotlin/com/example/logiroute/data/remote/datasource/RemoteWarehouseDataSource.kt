e5d22e56f08be0cfc87d6711371b8a26d9483cff
Musaab-Hisham <mosaabhisham2006@gmail.com>
2026-09-16 15:26:52 +0300

    feat: add warehouse crud remote interface

--- src/main/kotlin/com/example/logiroute/data/remote/datasource/RemoteWarehouseDataSource.kt
+++ src/main/kotlin/com/example/logiroute/data/remote/datasource/RemoteWarehouseDataSource.kt
@@ -1,7 +1,27 @@
 package com.example.logiroute.data.remote.datasource
 
+import com.example.logiroute.data.remote.dto.warehouse.CreateWarehouseRequestDto
+import com.example.logiroute.data.remote.dto.warehouse.UpdateWarehouseRequestDto
 import com.example.logiroute.data.remote.dto.warehouse.WarehouseResponseDto
 
 interface RemoteWarehouseDataSource {
-    fun getWarehouses(): List<WarehouseResponseDto>
+
+    suspend fun getWarehouses(): List<WarehouseResponseDto>
+
+    suspend fun getWarehouseById(
+        id: String
+    ): WarehouseResponseDto?
+
+    suspend fun createWarehouse(
+        request: CreateWarehouseRequestDto
+    ): WarehouseResponseDto
+
+    suspend fun updateWarehouse(
+        id: String,
+        request: UpdateWarehouseRequestDto
+    ): WarehouseResponseDto
+
+    suspend fun deleteWarehouse(
+        id: String
+    )
 }
